4863252d6cf22baa2f49c00eb8ae44239aedae39
Hisashi Takeda <tkdhss111@gmail.com>
2024-10-17 10:00:12 +0900

    minor

---
+++ build/test/CMakeFiles/unit_test.dir/__/src/metrics_mo.f90-pp.f90.d
@@ -0,0 +1,2 @@
+test/CMakeFiles/unit_test.dir/__/src/metrics_mo.f90-pp.f90: \
+ /home/hss/0_tkd/1_hss/2_tools/fortran-metrics/src/metrics_mo.f90
---
+++ build/test/CMakeFiles/unit_test.dir/unit_test.f90-pp.f90.d
@@ -0,0 +1,2 @@
+test/CMakeFiles/unit_test.dir/unit_test.f90-pp.f90: \
+ /home/hss/0_tkd/1_hss/2_tools/fortran-metrics/test/unit_test.f90
